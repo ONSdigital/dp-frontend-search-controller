0dc9a7acb839a3304a5a9b5a39d54934358c5c55
Liam Lewis <liamjlewis@gmail.com>
2022-02-10 11:41:55 +0000

    pointing to the latest dp-design-system

--- config/config.go
+++ config/config.go
@@ -37,7 +37,7 @@ func Get() (*Config, error) {
 	if cfg.Debug {
 		cfg.PatternLibraryAssetsPath = "http://localhost:9002/dist/assets"
 	} else {
-		cfg.PatternLibraryAssetsPath = "//cdn.ons.gov.uk/dp-design-system/5f3a539"
+		cfg.PatternLibraryAssetsPath = "//cdn.ons.gov.uk/dp-design-system/d4ace07"
 	}
 	return cfg, nil
 }
