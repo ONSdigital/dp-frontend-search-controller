bc8e0a02342ce83800fcf3efb5924ac1ae2cea2a
Liam Lewis <liamjlewis@gmail.com>
2022-03-02 20:38:04 +0000

    class name change and design system hash updated

--- config/config.go
+++ config/config.go
@@ -37,7 +37,7 @@ func Get() (*Config, error) {
 	if cfg.Debug {
 		cfg.PatternLibraryAssetsPath = "http://localhost:9002/dist/assets"
 	} else {
-		cfg.PatternLibraryAssetsPath = "//cdn.ons.gov.uk/dp-design-system/f283d14"
+		cfg.PatternLibraryAssetsPath = "//cdn.ons.gov.uk/dp-design-system/f333303"
 	}
 	return cfg, nil
 }
