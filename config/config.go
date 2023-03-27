0efc15986f87e13b3834b071fe280bfc7ee70463
lindenmckenzie <linden.mckenzie@ons.gov.uk>
2023-03-27 16:31:41 +0100

    Update design hash for 0 results title fix

--- config/config.go
+++ config/config.go
@@ -48,7 +48,7 @@ func Get() (*Config, error) {
 	if newCfg.Debug {
 		newCfg.PatternLibraryAssetsPath = "http://localhost:9002/dist/assets"
 	} else {
-		newCfg.PatternLibraryAssetsPath = "//cdn.ons.gov.uk/dp-design-system/22a3f50"
+		newCfg.PatternLibraryAssetsPath = "//cdn.ons.gov.uk/dp-design-system/4ff8af5"
 	}
 
 	return newCfg, nil
