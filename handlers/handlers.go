53fec20b1d5c9f072341cabd08814ba9046f5050
Andrea K <andrea.kyurchiev@flaxandteal.co.uk>
2023-10-31 15:12:44 +0200

    adds translations where needed

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -264,6 +264,7 @@ func readDataAggregation(w http.ResponseWriter, req *http.Request, cfg *config.C
 		defer wg.Done()
 
 		searchResp, respErr = searchC.GetSearch(ctx, options)
+		log.Info(ctx, "searchResp", log.Data{"searchResp": searchResp})
 		if respErr != nil {
 			log.Error(ctx, "getting search response from client failed", respErr)
 			cancel()
