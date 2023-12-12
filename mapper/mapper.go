8a45730b36d0bb721852d3240f50e8219be73916
lindenmckenzie <linden.mckenzie@ons.gov.uk>
2023-12-12 16:44:50 +0000

    Fix linting issues

--- mapper/mapper.go
+++ mapper/mapper.go
@@ -60,7 +60,7 @@ func CreateSearchPage(cfg *config.Config, req *http.Request, basePage coreModel.
 
 // CreateDataAggregationPage maps type searchC.Response to model.Page
 func CreateDataAggregationPage(cfg *config.Config, req *http.Request, basePage coreModel.Page,
-	validatedQueryParams data.SearchURLParams, categories []data.Category, topicCategories []data.Topic, populationTypes []data.PopulationTypes, dimensions []data.Dimensions,
+	validatedQueryParams data.SearchURLParams, categories []data.Category, topicCategories []data.Topic, _ []data.PopulationTypes, _ []data.Dimensions,
 	respC *searchModels.SearchResponse, lang string, homepageResponse zebedee.HomepageContent, errorMessage string,
 	navigationContent *topicModel.Navigation,
 	template string,
