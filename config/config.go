2b06276e05d6f7e63b2da3ba0a194e345cdbb349
Rhys <rhys.st.romaine@ons.gov.uk>
2024-03-14 09:43:44 +0000

    Update Pattern Library Assets Path

--- config/config.go
+++ config/config.go
@@ -62,7 +62,7 @@ func Get() (*Config, error) {
 	if newCfg.Debug {
 		newCfg.PatternLibraryAssetsPath = "http://localhost:9002/dist/assets"
 	} else {
-		newCfg.PatternLibraryAssetsPath = "//cdn.ons.gov.uk/dp-design-system/8e86405"
+		newCfg.PatternLibraryAssetsPath = "//cdn.ons.gov.uk/dp-design-system/dd9defe"
 	}
 
 	return newCfg, nil
