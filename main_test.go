e808c91353128e6935a4b4a23fcfb6e0cf2f042b
Nick Lupton <19624419+mr-nick17@users.noreply.github.com>
2024-04-12 14:00:03 +0100

    replace deprecated godog code

--- main_test.go
+++ main_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
 	"io"
@@ -32,13 +33,15 @@ func InitializeScenario(ctx *godog.ScenarioContext) {
 	apiFeature.RegisterSteps(ctx)
 	controllerComponent.RegisterSteps(ctx)
 
-	ctx.BeforeScenario(func(*godog.Scenario) {
+	ctx.Before(func(ctx context.Context, sc *godog.Scenario) (context.Context, error) {
 		uiFeature.Reset()
+		return ctx, nil
 	})
 
-	ctx.AfterScenario(func(*godog.Scenario, error) {
+	ctx.After(func(ctx context.Context, sc *godog.Scenario, err error) (context.Context, error) {
 		uiFeature.Close()
 		controllerComponent.Close()
+		return ctx, nil
 	})
 }
 
