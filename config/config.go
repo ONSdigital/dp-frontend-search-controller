d46410e08162f04b92aa83ffc5a236c9964f7a5d
Rhys <rhys.st.romaine@ons.gov.uk>
2024-05-23 14:20:20 +0100

    Update design system version

--- config/config.go
+++ config/config.go
@@ -64,7 +64,7 @@ func Get() (*Config, error) {
 	if newCfg.Debug {
 		newCfg.PatternLibraryAssetsPath = "http://localhost:9002/dist/assets"
 	} else {
-		newCfg.PatternLibraryAssetsPath = "//cdn.ons.gov.uk/dp-design-system/b5ef59e"
+		newCfg.PatternLibraryAssetsPath = "//cdn.ons.gov.uk/dp-design-system/bc5cfab"
 	}
 
 	return newCfg, nil
