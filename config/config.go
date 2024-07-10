1b383d1665bb7b4a980fa26f065cd81f47d8e501
lindenmckenzie <linden.mckenzie@ons.gov.uk>
2024-07-10 10:10:03 +0100

    Upgrade ds - guard for no RSS link

--- config/config.go
+++ config/config.go
@@ -64,7 +64,7 @@ func Get() (*Config, error) {
 	if newCfg.Debug {
 		newCfg.PatternLibraryAssetsPath = "http://localhost:9002/dist/assets"
 	} else {
-		newCfg.PatternLibraryAssetsPath = "//cdn.ons.gov.uk/dp-design-system/115736d"
+		newCfg.PatternLibraryAssetsPath = "//cdn.ons.gov.uk/dp-design-system/ba53f47"
 	}
 
 	return newCfg, nil
