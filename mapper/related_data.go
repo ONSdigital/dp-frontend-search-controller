ed3f2ebcae2dce6aaa35c771bdf3c98f513508b4
jondewijones <jondewijones@gmail.com>
2024-10-17 16:22:59 +0100

    add mapper for related data pages

---
+++ mapper/related_data.go
@@ -0,0 +1,45 @@
+package mapper
+
+import (
+	"net/http"
+
+	"github.com/ONSdigital/dp-api-clients-go/v2/zebedee"
+	"github.com/ONSdigital/dp-frontend-search-controller/cache"
+	"github.com/ONSdigital/dp-frontend-search-controller/config"
+	"github.com/ONSdigital/dp-frontend-search-controller/data"
+	"github.com/ONSdigital/dp-frontend-search-controller/model"
+	coreModel "github.com/ONSdigital/dp-renderer/v2/model"
+	searchModels "github.com/ONSdigital/dp-search-api/models"
+	topicModel "github.com/ONSdigital/dp-topic-api/models"
+)
+
+func CreateRelatedDataPage(cfg *config.Config, req *http.Request, basePage coreModel.Page,
+	validatedQueryParams data.SearchURLParams, respC *searchModels.SearchResponse, lang string, homepageResponse zebedee.HomepageContent, errorMessage string,
+	navigationContent *topicModel.Navigation, template string, topic cache.Topic, validationErrs []coreModel.ErrorItem, zebedeeResp zebedee.PageData, bc []zebedee.Breadcrumb,
+) model.SearchPage {
+	page := model.SearchPage{
+		Page: basePage,
+	}
+
+	page.Metadata.Title = "Data related to " + zebedeeResp.Description.Title
+	page.Title.Title = "All data related to " + zebedeeResp.Description.Title + ": " + zebedeeResp.Description.Edition
+	page.Metadata.Description = zebedeeResp.Description.MetaDescription
+	page.Type = "related-data"
+	page.Count = respC.Count
+	page.Language = lang
+	page.BetaBannerEnabled = true
+	page.SearchDisabled = false
+	page.Pagination.CurrentPage = validatedQueryParams.CurrentPage
+	page.ServiceMessage = homepageResponse.ServiceMessage
+	page.EmergencyBanner = mapEmergencyBanner(homepageResponse)
+
+	MapCookiePreferences(req, &page.Page.CookiesPreferencesSet, &page.Page.CookiesPolicy)
+
+	mapQuery(cfg, &page, validatedQueryParams, respC, *req, errorMessage)
+
+	mapResponse(&page, respC, []data.Category{})
+
+	mapBreadcrumb(&page, bc, zebedeeResp.Description.Title, zebedeeResp.URI)
+
+	return page
+}
