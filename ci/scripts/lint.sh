263f8992109d7f21d52d22c7d5a3e606858d960f
Ighomena Odebala <ighomena.odebala@ons.gov.uk>
2025-07-07 12:37:44 +0100

    Upgrade linter to v2

--- ci/scripts/lint.sh
+++ ci/scripts/lint.sh
@@ -1,7 +1,6 @@
 #!/bin/bash -eux
 
 pushd dp-frontend-search-controller
-  go install github.com/golangci/golangci-lint/cmd/golangci-lint@v1.64.6
   go install github.com/kevinburke/go-bindata/go-bindata
   make lint
 popd
