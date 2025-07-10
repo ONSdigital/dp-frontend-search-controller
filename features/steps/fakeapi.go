65ec3732d2ff64445be3f5aa696d52d02988cf53
Ighomena Odebala <ighomena.odebala@ons.gov.uk>
2025-07-10 11:53:59 +0100

    Use fake page data response instead of new handler

--- features/steps/fakeapi.go
+++ features/steps/fakeapi.go
@@ -18,6 +18,7 @@ type FakeAPI struct {
 	subSubTopicRequest      *httpfake.Request
 	navigationRequest       *httpfake.Request
 	previousReleasesRequest *httpfake.Request
+	pageDataRequest         *httpfake.Request
 	breadcrumbRequest       *httpfake.Request
 }
 
